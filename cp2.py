a187464c57d6646770b8f4fa6eac3d271c255e80
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-18 16:25:10 +0300

    Add script to analyze publications data and identify paper with most citations

---
+++ cp2.py
@@ -0,0 +1,8 @@
+df_json=pd.read_json('raw_data/publications.json')
+print (df_json.head())
+print(df_json.shape)
+print(df_json.dtypes)
+paper_most_citations = df_json.groupby('title')['citations'].max().sort_values(ascending=False).head(1)
+print("Paper with most citations:")
+print(paper_most_citations)
+# Data Opens Doors: A Manifesto for Open Research    9999
