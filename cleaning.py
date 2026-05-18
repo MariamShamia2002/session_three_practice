52f99c5fe28f47811840baa78de5db6297d2b4d1
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-18 14:45:34 +0300

    Refactor variable name for data frame in cleaning script

--- cleaning.py
+++ cleaning.py
@@ -1,17 +1,17 @@
 import pandas as pd
-
-data_researchers=pd.read_csv('raw_data/researchers.csv')
+#creat data frame
+df=pd.read_csv('raw_data/researchers.csv')
 print("Data frame shape:")
-print(data_researchers.shape)
+print(df.shape)
 print("Data frame first 5 rows:")
-print(data_researchers.head())
+print(df.head())
 print("Data frame  types columns:")
-print(data_researchers.dtypes)
+print(df.dtypes)
 print("Data frame last 5 rows:")
-print(data_researchers.tail())
+print(df.tail())
 print("Data frame summary statistics:")
-print(data_researchers.describe())
+print(df.describe())
 print("Data frame null values:")
-print(data_researchers.isnull().sum())
+print(df.isnull().sum())
 
 
