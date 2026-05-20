27c7d1c7163619eac318d4321ca9b7880c324b69
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-20 22:45:26 +0300

    Merge researchers + publications + funding into one unified DataFrame.

---
+++ merge.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df_researchers=pd.read_csv('raw_data/researchers.csv')
+print(df_researchers.shape) # (34 rows, 11 columns)
+df_funding=pd.read_excel('raw_data/funding.xlsx')
+print(df_funding.shape) # (29 rows, 7 columns)
+df_publications=pd.read_json('raw_data/publications.json')
+print(df_publications.shape) # (61 rows, 7 columns)
+merged_inner_df=pd.merge(df_researchers, df_publications ,on='researcher_id', how='inner')
+merged_inner_df=pd.merge(merged_inner_df, df_funding ,on='researcher_id', how='inner')
+# print(merged_inner_df.head())
+print(merged_inner_df.shape) # (49 rows, 23 columns) in the inner merge
+# the total number of unique researchers across all three datasets is 49, which is less than the sum of the individual dataset sizes (34 + 29 + 61 = 124) 
+# due to overlapping researcher_ids across the datasets.
+merged_left_df=pd.merge(df_researchers, df_publications ,on='researcher_id', how='left')
+merged_left_df=pd.merge(merged_left_df, df_funding ,on='researcher_id', how='left')
+# print(merged_left_df.head())
+print(merged_left_df.shape) # (86 rows, 23 columns) in the left merge
+# the total number of unique researchers across all three datasets is 86, which is equal to the number of researchers in the researchers dataset, 
+# as the left merge retains all rows from the left dataset (researchers) and includes matching rows from the publications and funding datasets where available. 
+# Researchers without matching entries in the publications or funding datasets will have NaN values in the corresponding columns. 
