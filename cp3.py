bbc22740a6e99aee85fb8ced9435bceaf206207b
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-20 22:55:58 +0300

    Wrap your funding cleaning logic into a reusable function clean_funding(df),handle nulls, negatives, and type conversion.

--- cp3.py
+++ cp3.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
-df_xlsx=pd.read_excel('raw_data/funding.xlsx', sheet_name='Sheet1')
-# print(df_xlsx.head())
-# print(df_xlsx.shape)
-# print(df_xlsx.dtypes)
-print("Null values in amount_cad column:")
-print(df_xlsx['amount_cad'].isnull().sum())
-df_xlsx['amount_cad'] =(df_xlsx['amount_cad'].fillna(0))
-df_xlsx =df_xlsx[df_xlsx['amount_cad'] > 0]
-df_xlsx['amount_cad'] = pd.to_numeric(df_xlsx['amount_cad'], errors='coerce')
-# print(df_xlsx.head(20))
-print("Total funding amount in CAD:")
-print(df_xlsx['amount_cad'].sum())
+df_funding=pd.read_excel('raw_data/funding.xlsx', sheet_name='Sheet1')
+# print(df_funding.head())
+# print(df_funding.shape)
+# print(df_funding.dtypes)
+# print("Null values in amount_cad column:")
+# print(df_funding['amount_cad'].isnull().sum())
+# df_funding['amount_cad'] =(df_funding['amount_cad'].fillna(0))
+# df_funding =df_funding[df_funding['amount_cad'] > 0]
+# df_funding['amount_cad'] = pd.to_numeric(df_funding['amount_cad'], errors='coerce')
+# print(df_funding.head(20))
+# print("Total funding amount in CAD:")
+# print(df_funding['amount_cad'].sum())
+
+def clean_funding_data(df):
+    df['amount_cad'] = df['amount_cad'].fillna(0)
+    df = df[df['amount_cad'] > 0]
+    df['amount_cad'] = pd.to_numeric(df['amount_cad'], errors='coerce')
+    return df
+print ("Total funding amount in CAD after cleaning:")
+print (clean_funding_data(df_funding)['amount_cad'].sum())
+# Total funding amount in CAD after cleaning:
+# 2024000.0
